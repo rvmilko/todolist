e772af6ab87560db6d59eaaad1d970cecce655f8
Roman Milko <rvmilko@gmail.com>
2023-10-27 20:26:34 +0300

    Starting security

---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Security;
+
+//use App\Entity\User;
+//use Doctrine\ORM\EntityManagerInterface;
+//use Symfony\Component\HttpFoundation\JsonResponse;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+//use Symfony\Component\Security\Core\Exception\AuthenticationException;
+//use Symfony\Component\Security\Core\User\UserInterface;
+//use Symfony\Component\Security\Core\User\UserProviderInterface;
+//use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator
+{
+
+}
